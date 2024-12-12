aa3310df17ddbfc0830719bd2b516f35b18832c9
harrykito <korhary@gmail.com>
2024-12-13 03:21:43 +0900

    update for test

---
+++ Safety_follow.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# For safety
+# 직접 비행모드 수정 절대 X
+
+from drone import drone
+
+# TODO
+if __name__ == "__main__":
+    vehicle = drone.drone()
+    print("For a safety flight!")
